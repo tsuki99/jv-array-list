2559dd792af27eda99c5a0aaea57eaea1a8f4ae3
Vlad Rublenko <vladik.rar@ukr.net>
2025-09-16 17:19:59 +0300

    initial commit

--- src/main/java/core/basesyntax/ArrayList.java
+++ src/main/java/core/basesyntax/ArrayList.java
@@ -1,48 +1,112 @@
 package core.basesyntax;
 
+import java.util.NoSuchElementException;
+
 public class ArrayList<T> implements List<T> {
+    private static final int STARTER_SIZE = 10;
+    private static final int STARTER_INDEX = 0;
+    private static final String INDEX_OUT_OF_BOUND_MESSAGE = " - index out of bound!";
+    private T[] storage;
+    private int size;
+
+    public ArrayList() {
+        storage = (T[]) new Object[STARTER_SIZE];
+    }
+
     @Override
     public void add(T value) {
-
+        growIfArrayFull();
+        storage[size] = value;
+        size++;
     }
 
     @Override
     public void add(T value, int index) {
-
+        if (index < STARTER_INDEX || index > size) {
+            throw new ArrayListIndexOutOfBoundsException(index + INDEX_OUT_OF_BOUND_MESSAGE);
+        } else {
+            growIfArrayFull();
+            System.arraycopy(storage, index, storage, index + 1, size - index);
+            storage[index] = value;
+            size++;
+        }
     }
 
     @Override
     public void addAll(List<T> list) {
-
+        for (int i = STARTER_INDEX; i < list.size(); i++) {
+            growIfArrayFull();
+            storage[size] = list.get(i);
+            size++;
+        }
     }
 
     @Override
     public T get(int index) {
-        return null;
+        if (checkIfIndexOfBounds(index)) {
+            throw new ArrayListIndexOutOfBoundsException(index + INDEX_OUT_OF_BOUND_MESSAGE);
+        } else {
+
+            return storage[index];
+        }
     }
 
     @Override
     public void set(T value, int index) {
-
+        if (checkIfIndexOfBounds(index)) {
+            throw new ArrayListIndexOutOfBoundsException(index + INDEX_OUT_OF_BOUND_MESSAGE);
+        } else {
+            storage[index] = value;
+        }
     }
 
     @Override
     public T remove(int index) {
-        return null;
+        if (checkIfIndexOfBounds(index)) {
+            throw new ArrayListIndexOutOfBoundsException(index + INDEX_OUT_OF_BOUND_MESSAGE);
+        } else {
+                final T removedValue = storage[index];
+                System.arraycopy(storage, index + 1, storage, index, size - index - 1);
+                size--;
+
+                return removedValue;
+        }
     }
 
     @Override
     public T remove(T element) {
-        return null;
+        for (int i = STARTER_INDEX; i < size; i++) {
+            if (checkIfElementExist(element, storage[i])) {
+                return remove(i);
+            }
+        }
+        throw new NoSuchElementException("No such element");
     }
 
     @Override
     public int size() {
-        return 0;
+        return size;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return size == 0;
+    }
+
+    private void growIfArrayFull() {
+        if (size == storage.length) {
+            T[] updatedArray = (T[]) new Object[(int) (size * 1.5)];
+            System.arraycopy(storage, STARTER_INDEX, updatedArray, STARTER_INDEX, size);
+            storage = updatedArray;
+        }
+    }
+
+    private boolean checkIfIndexOfBounds(int index) {
+        return index < STARTER_INDEX || index >= size;
+    }
+
+    private boolean checkIfElementExist(T element, T elementFromStorage) {
+        return elementFromStorage == element
+                || elementFromStorage != null && elementFromStorage.equals(element);
     }
 }
